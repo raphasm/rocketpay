6ab895a8a4f027627c40846687b94916aaa11682
Raphael Margoni <raphasm.dev@hotmail.com>
2022-11-10 15:10:43 -0300

    segundo-commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
-import "./css/index.css"
+import './css/index.css'
+
+const ccColor1 = document.querySelector('.cc-bg svg g > path ')
+
+const ccColor2 = document.querySelector('.cc-bg svg g g:nth-child(2) path ')
+
+const imgCard = document.querySelector('.cc-logo span img')
+
+// Passando o type como parametro para usar ele para referenciar as propriedades do obj
+function setCardColor(type) {
+  const colors = {
+    visa: ['#2D57F2', '#436D99'],
+    mastercard: ['#C69347', '#DF6F29'],
+    default: ['black', 'gray']
+  }
+  console.log([type][0]) // usando o parametro type para pegar as propriedades e as posiçoes delas
+  ccColor1.setAttribute('fill', colors[type][0]) // quando usa '[type]' referencia as propriedades va 'colors'
+  ccColor2.setAttribute('fill', colors[type][1])
+  imgCard.setAttribute('src', `cc-${type}.svg`)
+}
+
+setCardColor('visa')
